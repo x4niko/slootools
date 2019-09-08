f0f2ae73219df0971fa5735b1296c86afc479ad9
x4niko <x4niko@163.com>
2019-09-08 23:23:03 +0800

    add vue support

---
+++ src/main.js
@@ -0,0 +1,11 @@
+
+var app = new Vue({
+    el: '#placeholder',
+    // components: {
+    //   // <app-header> 只在父模板可用
+    //   'app-header': Header
+    // },
+    data: {
+      message: ''
+    }
+})
---
+++ src/navigation.js
@@ -0,0 +1,8 @@
+
+Vue.component('app-navi', {
+    template: '#navi-template'
+})
+
+var app = new Vue({
+    el: '#app-header'
+})
